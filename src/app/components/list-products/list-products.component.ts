3da675901b26355531c28cd3af3a5b79f99ac44c
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-06 05:39:28 +0200

    NEW - Agregando el routing de los componentes

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/product';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './list-products.component.html',
   styleUrl: './list-products.component.css'
 })
 export class ListProductsComponent {
+
+  listProducts: Product[] = [
+    { 
+      id: 1,
+      name: 'Coca Cola', 
+      description: 'Bebida azucarada', 
+      price: 4, 
+      stock: 500
+    },
+    { 
+      id: 2,
+      name: 'Ceres', 
+      description: 'Bebida con alcohol', 
+      price: 10, 
+      stock: 200
+    },
+  ]
 
 }
