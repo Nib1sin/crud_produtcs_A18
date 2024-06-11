6d4d3d4287306e8bbbcc96a502ecd4d6111e7aa5
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-11 22:52:07 +0200

    ENH - Agregando el get && put

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { nonNegativePriceValidator } from '../../utils/validators';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../interfaces/product';
@@ -19,22 +19,64 @@ export class AddEditProductComponent {
 
   //[x: string]: any;
   form: FormGroup;
-  loading: boolean= false;
+  loading: boolean = false;
+  id: number;
+  operacion: string = 'Agregar ';
 
   constructor(
+    //Injeccion de clases
     private fb: FormBuilder,
     private _productService: ProductService,
-    //Injeccion con typeScript
+    //Injeccion router con typeScript
     private router: Router,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private aRouter: ActivatedRoute
   ){
     this.form = this.fb.group({
       name: ['', [Validators.required, Validators.maxLength(30)]],
       description: ['', Validators.required],
       price: [0, [Validators.required, nonNegativePriceValidator]],
       stock: [null, Validators.required],
-    });
+    })
+
+    //si aRouter.snapshot.paramMap.get('id') es undefined, entonces Number es 0
+    this.id = Number(aRouter.snapshot.paramMap.get('id'))
+
+    //Capturamos el valor de la url
+    //aRouter.snapshot.paramMap.get('id');
   }
+
+
+  ngOnInit(): void{
+    if(this.id != 0) {
+      //es editar
+      this.operacion = 'Editar ';
+      this.getProduct(this.id);
+    }
+  }
+
+  getProduct(id: number){
+    this.loading = true;
+    //modifico el producto
+    this._productService.getProduct(id).subscribe( (data:Product) => {
+      this.loading = false;
+      /*
+      //De esta forma, modificamos solo las propiedades definidas
+      this.form.patchValue({
+        name: data.name
+      })
+      */
+
+      //De esta manera modificamos todo el objeto, definiendo cada pripiedad del eleemento
+      this.form.setValue({
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.description,
+      })
+    })
+  }
+
 
   addProduct(): void {
     //console.log(this.form);
@@ -46,15 +88,25 @@ export class AddEditProductComponent {
       stock:        this.form.value.stock
     }
 
-
     if (this.form.valid) {
       this.loading = true;
-      this._productService.saveProduct(product).subscribe(()=> {
-        this.loading = false;
-        this.toastr.success(`El producto ${product.name} ha sido agregado`, 'Producto Registrado');
-        this.router.navigate(['/']);
-      })
+      
+      if(this.id !== 0){
+        product.id = this.id;
+        this._productService.saveProduct(product).subscribe(()=> {
+          this.toastr.info(`El producto ${product.name} fue actualizado con exito.`, 'Producto Actualizado');
+          this.loading = false;
+          this.router.navigate(['/']);})
+      } else {
+        this._productService.saveProduct(product).subscribe(()=> {
+          this.toastr.success(`El producto ${product.name} fue registrado con exito.`, 'Producto Registrado');
+          this.loading = false;
+          this.router.navigate(['/']);
+        })
+      }
+
     }
+
   }
 
 }
